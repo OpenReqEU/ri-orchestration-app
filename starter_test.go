2d5ebc05f9446a56fd3576eeed2f1c887c112d81
Tim Pietz <tim@pietz.me>
2019-11-25 17:14:44 +0100

    Refactor test code

--- starter_test.go
+++ starter_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io"
+	"github.com/pkg/errors"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -35,76 +37,66 @@ func tearDown() {
 	fmt.Println("--- --- tear down")
 }
 
-func buildRequest(method, endpoint string, payload io.Reader, t *testing.T) *http.Request {
-	req, err := http.NewRequest(method, endpoint, payload)
-	if err != nil {
-		t.Errorf("An error occurred. %v", err)
-	}
-
-	return req
+type endpoint struct {
+	method string
+	url    string
 }
 
-func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+func (e endpoint) withVars(vs ...interface{}) endpoint {
+	e.url = fmt.Sprintf(e.url, vs...)
+	return e
+}
+
+func (e endpoint) executeRequest(payload interface{}) (error, *httptest.ResponseRecorder) {
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(payload)
+	if err != nil {
+		return err, nil
+	}
+
+	req, err := http.NewRequest(e.method, e.url, body)
+	if err != nil {
+		return err, nil
+	}
+
 	rr := httptest.NewRecorder()
 	router.ServeHTTP(rr, req)
+
+	return nil, rr
+}
+
+func (e endpoint) mustExecuteRequest(payload interface{}) *httptest.ResponseRecorder {
+	err, rr := e.executeRequest(payload)
+	if err != nil {
+		panic(errors.Wrap(err, `Could not execute request`))
+	}
+
 	return rr
 }
 
-func TestPostObserveAppGooglePlay(t *testing.T) {
-	fmt.Println("start TestPostObserveAppGooglePlay")
-	var method = "POST"
-	var endpoint = "/hitec/orchestration/app/observe/google-play/package-name/%s/interval/%s"
+func isSuccess(code int) bool {
+	return code >= 200 && code < 300
+}
 
-	/*
-	 * test for faillure
-	 */
-	endpointFail := fmt.Sprintf(endpoint, "", "fail")
-	req := buildRequest(method, endpointFail, nil, t)
-	rr := executeRequest(req)
-
-	//Confirm the response has the right status code
-	if status := rr.Code; status != http.StatusMovedPermanently {
-		t.Errorf("Status code differs. Expected %d .\n Got %d instead", http.StatusMovedPermanently, status)
+func assertSuccess(t *testing.T, rr *httptest.ResponseRecorder) {
+	if !isSuccess(rr.Code) {
+		t.Errorf("Status code differs. Expected success.\n Got status %d (%s) instead", rr.Code, http.StatusText(rr.Code))
 	}
-
-	/*
-	 * test for success
-	 */
-	endpointSuccess := fmt.Sprintf(endpoint, "eu.openreq", "monthly")
-	req = buildRequest(method, endpointSuccess, nil, t)
-	rr = executeRequest(req)
-
-	//Confirm the response has the right status code
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Status code differs. Expected %d .\n Got %d instead", http.StatusOK, status)
+}
+func assertFailure(t *testing.T, rr *httptest.ResponseRecorder) {
+	if isSuccess(rr.Code) {
+		t.Errorf("Status code differs. Expected failure.\n Got status %d (%s) instead", rr.Code, http.StatusText(rr.Code))
 	}
 }
 
+func TestPostObserveAppGooglePlay(t *testing.T) {
+	ep := endpoint{method: "POST", url: "/hitec/orchestration/app/observe/google-play/package-name/%s/interval/%s"}
+	assertFailure(t, ep.withVars("", "fail").mustExecuteRequest(nil))
+	assertSuccess(t, ep.withVars("eu.openreq", "monthly").mustExecuteRequest(nil))
+}
+
 func TestPostProcessTweets(t *testing.T) {
-	fmt.Println("start TestPostProcessTweets")
-	var method = "POST"
-	var endpoint = "/hitec/orchestration/app/process/google-play/package-name/%s"
-	/*
-	 * test for faillure
-	 */
-	endpointFail := fmt.Sprintf(endpoint, "")
-	req := buildRequest(method, endpointFail, nil, t)
-	rr := executeRequest(req)
-
-	//Confirm the response has the right status code
-	if status := rr.Code; status != http.StatusNotFound {
-		t.Errorf("Status code differs. Expected %d .\n Got %d instead", http.StatusNotFound, status)
-	}
-
-	/*
-	 * test for success
-	 */
-	endpointSuccess := fmt.Sprintf(endpoint, "eu.openreq")
-	req = buildRequest(method, endpointSuccess, nil, t)
-	rr = executeRequest(req)
-
-	//Confirm the response has the right status code
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Status code differs. Expected %d .\n Got %d instead", http.StatusOK, status)
-	}
+	ep := endpoint{method: "POST", url: "/hitec/orchestration/app/process/google-play/package-name/%s"}
+	assertFailure(t, ep.withVars("").mustExecuteRequest(nil))
+	assertSuccess(t, ep.withVars("eu.openreq").mustExecuteRequest(nil))
 }
